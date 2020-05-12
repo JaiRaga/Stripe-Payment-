a0c97f4806db74cedf442a97832f9b2cd700470c
JaiRaga <raga.j.santhosh@gmail.com>
2020-05-12 20:29:49 +0530

    fourth commit

--- server.js
+++ server.js
@@ -1,3 +1,11 @@
+if (process.env.NODE_ENV !== "production") {
+  require("dotenv").config();
+}
+
+const STRIPE_SECRET_KEY = process.env.STRIPE_SECRET_KEY;
+
+console.log(STRIPE_SECRET_KEY);
+
 const express = require("express");
 const app = express();
 
